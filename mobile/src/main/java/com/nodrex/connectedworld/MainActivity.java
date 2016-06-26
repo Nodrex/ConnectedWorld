d100a3b538ac4d42a58851a1574f69315a7ed810
Nodar Tchumbadze <Nodar Tchumbadze>
2016-06-26 13:29:20 +0400

    removed unused imports

--- mobile/src/main/java/com/nodrex/connectedworld/MainActivity.java
+++ mobile/src/main/java/com/nodrex/connectedworld/MainActivity.java
@@ -20,7 +20,6 @@ import android.view.View;
 import android.view.ViewStub;
 import android.view.animation.AnimationUtils;
 import android.view.animation.LinearInterpolator;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ImageView;
 
