b2f1007a7cc860c08d384893e3a3d8c95084d51c
Nodar Tchumbadze <Nodar Tchumbadze>
2016-10-08 01:23:07 +0400

    added some gradle params

--- mobile/src/main/java/com/nodrex/connectedworld/helper/Helper.java
+++ mobile/src/main/java/com/nodrex/connectedworld/helper/Helper.java
@@ -1,18 +1,5 @@
 package com.nodrex.connectedworld.helper;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
-import android.view.View;
-
-import com.nodrex.android.tools.Util;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Set;
-import java.util.UUID;
-
 /**
  * Helper class.
  * @author Nodar Tchumbadze
